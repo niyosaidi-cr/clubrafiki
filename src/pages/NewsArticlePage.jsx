91bd7063b85fc70fb0f06e675f07c78ae696ceec
niyosaidi-cr <niyosaidi@gmail.com>
2026-09-11 11:11:47 +0200

    Add like button linking to Flickr photo

--- src/pages/NewsArticlePage.jsx
+++ src/pages/NewsArticlePage.jsx
@@ -132,6 +132,16 @@ export default function NewsArticlePage({ article, onBack }) {
                 <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"/>
               </svg>
             </a>
+            <a
+              href="https://www.flickr.com/photos/paulkagame/54697022511/in/album-72177720328145264"
+              target="_blank" rel="noreferrer"
+              className="w-8 h-8 rounded-full bg-white shadow-sm flex items-center justify-center hover:bg-red-500 hover:text-white transition-colors text-red-500"
+              title="Like this photo"
+            >
+              <svg viewBox="0 0 24 24" className="w-4 h-4 fill-current">
+                <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
+              </svg>
+            </a>
           </div>
         </div>
       </div>
